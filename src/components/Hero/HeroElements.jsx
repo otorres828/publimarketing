8118817828cc8cdb21dc8d22aecd2e84a9a989fb
Oliver Torres <olivertorres1997@gmail.com>
2023-08-19 19:32:57 -0400

    subida

--- src/components/Hero/HeroElements.jsx
+++ src/components/Hero/HeroElements.jsx
@@ -6,13 +6,13 @@ export const HeroContainer = styled.div`
 		url(${ImgBg});
 	height: 100vh;
 	background-position: center;
-	background-size: cover;
+	background-size: contain;
 `;
 
 export const HeroContent = styled.div`
 	height: calc(100vh-80px);
 	max-height: 100%;
-	/* width: 100vw; */
+ width: 100vw; 
 	padding: 0rem calc((100vw - 1300px) / 2);
 `;
 
